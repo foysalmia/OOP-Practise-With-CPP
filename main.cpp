d50a072aaeb1c9c539bfb022a9e84f3d9c556fd5
foysalmia <mfoysal314@gmail.com>
2022-07-19 02:03:22 +0600

    task 9,10,11,12 all are done with no pera

--- main.cpp
+++ main.cpp
@@ -12,15 +12,41 @@ public:
     string name;
     char section;
     int roll;
-    set_marks(int a){
+    set_marks(int a)
+    {
         english_marks = a;
     };
-    set_password(string s){
+    set_password(string s)
+    {
         password = s;
-    }
-    int get_maks(){
+    };
+    int get_maks()
+    {
         return english_marks;
+    };
+    update_marks(string pass,int mark)
+    {
+        if(pass == password)
+        {
+            english_marks = mark;
+        }
+        else
+        {
+            cout<<"Sorry, Password doesn't matched !!"<<endl;
+        }
+    };
+    get_student(int r,string pass,int mark)
+    {
+        if(r==roll)
+        {
+            update_marks(pass,mark);
+        }
+        else
+        {
+            cout<<"Not found !!"<<endl;
+        }
     }
+
 
 };
 
@@ -32,12 +58,27 @@ int main()
 
     Student students[N];
 
-    for(int i=0;i<N;i++){
+    cout<<"Input Name Section Roll Mark Password"<<endl;
+    for(int i=0; i<N; i++)
+    {
         int mark;
         string pass;
         cin>>students[i].name>>students[i].section>>students[i].roll>>mark>>pass;
         students[i].set_marks(mark);
         students[i].set_password(pass);
+    }
+
+    cout<<"Update Students Data with {Std No. Roll mark Password} "<<endl;
+    int n,r,mark;
+    string p;
+    cin>>n>>r>>mark>>p;
+    students[n-1].get_student(r,p,mark);
+
+
+
+    cout<<"*********************************************"<<endl;
+    for(int i=0;i<N;i++){
+        cout<<students[i].name<<" "<<students[i].section<<" "<<students[i].roll<<" "<<students[i].get_maks()<<endl;
     }
 
     return 0;
