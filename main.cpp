b47e21b6898ba08e6eb561bb9c1d945a7080b4b6
foysalmia <mfoysal314@gmail.com>
2022-07-19 01:23:01 +0600

    task 2,3,4,5,6,7 done

--- main.cpp
+++ main.cpp
@@ -4,18 +4,33 @@ using namespace std;
 
 class Student
 {
+private:
+    string password;
+protected:
+    int english_marks;
+public:
     string name;
     char section;
     int roll;
-    int english_marks;
-    string password;
+    set_marks(int a){
+        english_marks = a;
+    };
+    set_password(string s){
+        password = s;
+    }
+    int get_maks(){
+        return english_marks;
+    }
+
 };
 
 
-int main(){
+int main()
+{
     int N;
     cin>>N;
 
     Student students[N];
+
     return 0;
 }
