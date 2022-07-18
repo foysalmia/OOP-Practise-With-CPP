0727fe174767a276d84bf72fd9e660255b0b4ede
foysalmia <mfoysal314@gmail.com>
2022-07-19 01:12:53 +0600

    task 1 done

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Student
+{
+    string name;
+    char section;
+    int roll;
+    int english_marks;
+    string password;
+};
+
+
+int main(){
+    int N;
+    cin>>N;
+
+    Student students[N];
+    return 0;
+}
